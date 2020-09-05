3322bdf3fc1aec764461aadcfba2c07045fa18aa
Jeremy Bailey <jeremylumanbailey@gmail.com>
2020-09-04 20:07:20 -0400

    script auto runs and shuts down after a little under an hour, everything seems to be working

--- wrapper.js
+++ wrapper.js
@@ -4,15 +4,14 @@ const {  performance } = require('perf_hooks');
 
 console.log("\nscript started\n");
 
-var exec = require('child_process').exec, child;
-child = exec('/usr/bin/java -jar  /home/jeremy/Downloads/mctmp/server.jar nogui',
-  function (error, stdout, stderr){
-    // console.log('stdout: ' + stdout);
-    // console.log('stderr: ' + stderr);
-    shutDownMachine();
-    if(error !== null){
-      console.log('exec error: ' + error);
-    }
+var exec = require('child_process').exec;
+var child = exec('/usr/bin/java -jar /home/ec2-user/endtimespart2/mcserver/server.jar nogui', function (error, stdout, stderr){
+  // console.log('stdout: ' + stdout);
+  console.log('stderr: ' + stderr);
+  shutDownMachine();
+  if(error !== null){
+    console.log('exec error: ' + error);
+  }
 });
 
 child.stdin.setEncoding('utf-8');
@@ -31,9 +30,21 @@ function writeToChildConsole(str) {
 
 }
 
- const shutDownMachine = () => {
+ function shutDownMachine() {
 
-  console.log("Shutting down computer.")
+  console.log("Shutting down computer.");
+
+
+   var killer = exec('sudo shutdown now', function (error, stdout, stderr){
+	  console.log('stdout: ' + stdout);
+  if(error !== null){
+    console.log('exec error: ' + error);
+  }
+	});
+
+
+
+
   exit();
 
 
@@ -77,103 +88,48 @@ recursiveAsyncReadLine(); //we have to actually start our recursion somehow
 let amountOfTimeBeforeCheckingPlayerCount = 30000;
 setTimeout(startCheckingPlayerCount, amountOfTimeBeforeCheckingPlayerCount);
 
-function startCheckingPlayerCount() {
+function checkIfPlayersOnline(err, res) {
+  if (err) {
+      // Some kind of error
+      console.error(err);
+  } else {
+    // Success!
+    console.log(res);
 
-  var amountOfTimeWithoutPlayersOnline, tmpTime;
-  var endTime, startTime = performance.now();
-  var minutes = 1, the_interval = minutes * 60 * 1000;
-  the_interval = 1000 // 1000 / 1 second
-  var timeUntilShutdownWhenNoneOnline = 2500; //2500 = 25 seconds
-  var refreshIntervalId = setInterval(function() {
-    
-    mcping('localhost', 25565, function(err, res) {
-      if (err) {
-          // Some kind of error
-          console.error(err);
-          
-      } else {
-          // Success!
-          console.log(res);
-
-          endTime = performance.now();
-          amountOfTimeWithoutPlayersOnline = endTime - startTime; //in ms 
-          console.log(amountOfTimeWithoutPlayersOnline);
-          if(amountOfTimeWithoutPlayersOnline >= 25000 && res.players.online == 0 ) {
-            writeToChildConsole("say No players detected on server. Server is shutting down.");
-            console.log("No players detected on server for extendend period of time. Server is shutting down");
-            writeToChildConsole('stop');
-            child.stdin.end();
-            rl.close();
-            clearInterval(refreshIntervalId);
-          }
-          else if(res.players.online == 0 ) {
-            if(tmpTime != startTime) {
-            startTime = performance.now();
-            tmpTime = startTime;
-            }
-          }
-          else {
-            tmpTime = 0;
-            startTime = endTime;
-          }
+    this.endTime = performance.now();
+    this.amountOfTimeWithoutPlayersOnline = this.endTime - this.startTime; //in ms 
+    console.log(this.amountOfTimeWithoutPlayersOnline);
+    if (this.amountOfTimeWithoutPlayersOnline >= 3240000 && res.players.online == 0) {
+      writeToChildConsole("say No players detected on server. Server is shutting down.");
+      console.log("No players detected on server for extendend period of time. Server is shutting down");
+      writeToChildConsole('stop');
+      child.stdin.end();
+      rl.close();
+     //  clearInterval(refreshIntervalId);
+    }
+    else if (res.players.online == 0) {
+      if(this.tmpTime != this.startTime) {
+        this.startTime = performance.now();
+        this.tmpTime = this.startTime;
       }
-    }, 3000);
+    }
+    else {
+      this.tmpTime = 0;
+      this.startTime = this.endTime;
+    }
+  }
+}
 
-    
+function startCheckingPlayerCount() {
+  this.startTime = performance.now();
+  this.amountOfTimeWithoutPlayersOnline = 0;
+  this.tmpTime = 0;
+  var minutes = 1
+  this.timeUntilShutdownWhenNoneOnline = 2500; //2500 = 25 seconds
+  var the_interval = minutes * 60 * 1000;
+  the_interval = 1000 // 1000 / 1 second
+  setInterval(() => {
+    mcping('localhost', 25565, checkIfPlayersOnline.bind(this), 3000);  
   }, the_interval);
 }
 
-
-/* DEAD CODE DEAD CODE DEAD CODE DEAD CODE DEAD CODE DEAD CODE DEAD CODE DEAD CODE
-
-console.log("current player count is: " + currentPlayerCount);
-  console.log("and the counter is set at: " + counter);
-  if(counter > 2 && currentPlayerCount === 0) {
-    // writeToChildConsole("say server will be shutting down soon")
-    console.log("exiting ")
-    writeToChildConsole('stop');
-    child.stdin.end();
-    rl.close();
-    clearInterval(refreshIntervalId);
-  }
-  if(currentPlayerCount === 0) {
-    writeToChildConsole("say server will be shutting down soon")
-    counter++;
-  }
-  
-  // do your stuff here
-
-
-// setTimeout(fun, 30000);
-
-//   var exec = require('child_process').exec, childKiller, ;
-
-//   childKiller = exec('shutdown now',
-//   function (error, stdout, stderr){
-//     // console.log('stdout: ' + stdout);
-//     // console.log('stderr: ' + stderr);
-//     console.log("server has shutdown. Turning off machine.");
-//     setTimeout(fun, 30000);
-//     if(error !== null){
-//       console.log('exec error: ' + error);
-//     }
-// });
-
-// const { spawn } = require('child_process');
-
-// var spawn = require('child_process').spawn,
-//     ls    = spawn('ls', ['-lh', '/usr']);
-
-// ls.stdout.on('data', function (data) {
-//   console.log('stdout: ' + data.toString());
-// });
-
-// ls.stderr.on('data', function (data) {
-//   console.log('stderr: ' + data.toString());
-// });
-
-// ls.on('exit', function (code) {
-//   console.log('child process exited with code ' + code.toString());
-// });
-
-*/
